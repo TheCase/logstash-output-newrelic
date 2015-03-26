407ce99f26ce1aa286645512efd89b0eb8efa162
Seth Schwartzman <seth@newrelic.com>
2015-03-25 22:43:04 -0400

    updated rspec

--- spec/outputs/newrelic_spec.rb
+++ spec/outputs/newrelic_spec.rb
@@ -6,16 +6,23 @@ require "logstash/event"
 # TO DO: Well... most of it. This is just the start.
 
 describe LogStash::Outputs::NewRelic do
-  let(:sample_event) { LogStash::Event.new }
-  let(:output) { LogStash::Outputs::NewRelic.new }
-  let(:minimal_settings)  {  { "account_id" => "284929",
-                               "insert_key" => "BYh7sByiVrkfqcDa2eqVMhjxafkdyuX0" } }
 
+  let (:simple_event) { LogStash::Event.new( {  'message' => 'hello', 'topic_name' => 'my_topic', 'host' => '172.0.0.1',
+												'@timestamp' => LogStash::Timestamp.now } ) }
+  let(:output) { LogStash::Outputs::NewRelic.new }
+  let(:options)  {  { "account_id" => "284929",
+                      "insert_key" => "BYh7sByiVrkfqcDa2eqVMhjxafkdyuX0" } }
+                      
+  it "should register" do
+    output = LogStash::Plugin.lookup("output", "newrelic").new(options)
+    expect {output.register}.to_not raise_error
+  end
+  
   before do
     output.register
   end
 
-  describe "receive message" do
-    subject { output.receive(sample_event) }
+  describe "::receive" do
+    subject { output.receive(simple_event) }
   end
 end
