7fbe29e7e3b10237b52745e607856dbf489c578d
Seth Schwartzman <seth@newrelic.com>
2015-03-26 21:32:16 -0400

    First functional rspec!

--- spec/outputs/newrelic_spec.rb
+++ spec/outputs/newrelic_spec.rb
@@ -1,28 +1,50 @@
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/newrelic"
-require "logstash/codecs/plain"
 require "logstash/event"
 
-# TO DO: Well... most of it. This is just the start.
+# TO DO (in order of easiest to hardest):
+# + Test batch_timeout
+# + Test 3 different kinds of "Insights Reserved Words": Moved, Backticks and Other
+# + Test various timestamp formats
+# + Test proxy (oy gevalt)
 
 describe LogStash::Outputs::NewRelic do
 
   let (:simple_event) { LogStash::Event.new( {  'message' => 'hello', 'topic_name' => 'my_topic', 'host' => '172.0.0.1',
-												'@timestamp' => LogStash::Timestamp.now } ) }
-  let(:output) { LogStash::Outputs::NewRelic.new }
-  let(:options)  {  { "account_id" => "284929",
+                        'timestamp' => LogStash::Timestamp.now } ) }  
+  let(:options) { { "account_id" => "284929",
                       "insert_key" => "BYh7sByiVrkfqcDa2eqVMhjxafkdyuX0" } }
                       
-  it "should register" do
-    output = LogStash::Plugin.lookup("output", "newrelic").new(options)
-    expect {output.register}.to_not raise_error
+  describe "#register" do
+    it "should register" do
+      output = LogStash::Plugin.lookup("output", "newrelic").new(options)
+      expect { output.register }.to_not raise_error
+    end
+    
+    it "should NOT register when batch_events > 1000" do
+      options["batch_events"] = 1001
+      output = LogStash::Plugin.lookup("output", "newrelic").new(options)
+      expect { output.register }.to raise_error
+    end
   end
-  
-  before do
-    output.register
-  end
-
-  describe "::receive" do
-    subject { output.receive(simple_event) }
+ 
+  describe "#receive" do
+    it "should send a single event" do
+      options["batch"] = false
+      output = LogStash::Plugin.lookup("output", "newrelic").new(options)
+      output.register
+      expect { output.receive(simple_event) }.to_not raise_error
+    end
+    
+    it "should send multiple events" do
+      batch_event_count = 5
+      options["batch_events"] = batch_event_count
+      output = LogStash::Plugin.lookup("output", "newrelic").new(options)
+      output.register
+      for i in 0..batch_event_count
+        simple_event["iteration"] = i
+        expect { output.receive(simple_event) }.to_not raise_error
+      end
+    end
   end
 end
